8c55c76c0d6e3c32a1b1a7f94ee8997ec64ce4f3
fatima-ola <fatimaolasunkanmi01@gmail.com>
2021-12-27 15:24:53 +0100

    destructure props and remove unuse files

--- src/components/Poster.js
+++ src/components/Poster.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 const Poster = (props) => {
+  const { img, title, text } = props;
   return (
     <div className="wrapper">
       <div className="child-container">
-        <img src={props.img} alt="react-logo" />
-        <h2 className="title">{props.title}</h2>
-        <p className="quote-text">{props.text}</p>
+        <img src={img} alt="react-logo" />
+        <h2 className="title">{title}</h2>
+        <p className="quote-text">{text}</p>
       </div>
     </div>
   );
